251d5a167b7670559df9f1d22b73a6edbb29c8b2
YAlambasha <103192982+YAlambasha@users.noreply.github.com>
2024-08-22 17:13:40 +0530

    final commit

--- index.js
+++ index.js
@@ -6,14 +6,67 @@ let w_temperature=document.querySelector(".weather_temperature");
 let w_minTem=document.querySelector(".weather_min");
 let w_maxTem=document.querySelector(".weather_max");
 
+let w_feelsLike=document.querySelector(".weather_feelsLike");
+let w_humidity=document.querySelector(".weather_humidity");
+let w_wind=document.querySelector(".weather_wind");
+let w_pressure=document.querySelector(".weather_pressure");
+let citySearch=document.querySelector(".weather_search")
+
+//to get actual country name
+const getCountryName=(code)=>{
+return new Intl.DisplayNames([code],{type:"region"}).of(code);
+}
+const getDateTime=(dt)=>{
+    const curDate=new Date(dt *1000);
+    console.log(curDate);
+
+const options={
+    weekday:"long",
+    year:"numeric",
+    month:"long",
+    day:"numeric",
+    hour:"numeric",
+    minute:"numeric",
+};
+const formatter=new Intl.DateTimeFormat("en-Us",options);
+return formatter.format(curDate);
+}
+
+let city="pune";
+//search functionality
+citySearch.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let cityName=document.querySelector(".city_name");
+    console.log(cityName.value);
+    city=cityName.value;
+    getWeatherData();
+    cityName.value="";
+});
+
 const getWeatherData=async()=>{
-   const weatherUrl=`https://api.openweathermap.org/data/2.5/weather?q=pune&APPID=79151b701915236b160c68a2b54f26e9`
+   const weatherUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=79151b701915236b160c68a2b54f26e9`
     try{
     const res=await fetch(weatherUrl);
     const data=await res.json();
     
 
     const{main,name,weather,wind,sys,dt}=data;
+    cityName.innerHTML=`${name},${getCountryName(sys.country)}`;
+    dateTime.innerHTML=getDateTime(dt);
+   
+    w_forecast.innerHTML=weather[0].main;
+    w_icon.innerHTML=`<img src="https://openweathermap.org/img/wn/${weather[0].icon}@4x.png"/>`;
+
+
+    w_temperature.innerHTML=`${main.temp}&#176`;
+    w_minTem.innerHTML=`Min: ${main.temp_min.toFixed()}&#176`;
+    w_maxTem.innerHTML=`Max: ${main.temp_max.toFixed()}&#176`;
+
+    w_feelsLike.innerHTML=`${main.feels_like.toFixed(2)}&#176`;
+    w_humidity.innerHTML=`${main.humidity}%`;
+    w_wind.innerHTML=`${wind.speed}m/s`;
+    w_pressure.innerHTML=`${main.pressure}hpa`;
+    
     }catch(error){
         console.log(error)
     }
